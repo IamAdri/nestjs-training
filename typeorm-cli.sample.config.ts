3ebc2a52993b617f9d2ded24535261a6195bcf7c
IAmAdri <atoma304@gmail.com>
2026-03-17 15:28:46 +0200

    Added a sample for typeorm-cli.config file

--- typeorm-cli.sample.config.ts
+++ typeorm-cli.sample.config.ts
@@ -3,10 +3,10 @@ import { DataSource } from "typeorm";
 export default new DataSource({
     type: 'postgres',
     host: 'localhost',
-    port: 5433,
-    username: 'postgres',
-    password: 'IKnowNestjs26!',
-    database: 'nestjs-blog',
+    port: 5432,
+    username: "USERNAME",
+    password: 'PASSWORD',
+    database: 'DATABASE',
     entities: ['**/*.entity.js'],
     migrations: ['migrations/*.js']
 })
