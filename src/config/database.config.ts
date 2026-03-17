26aea34d7627416543ad4cab51d1361c7183ec36
IAmAdri <atoma304@gmail.com>
2026-03-17 21:43:10 +0200

    Changed database.config port value

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,8 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   host: process.env.DATABASE_HOST || 'localhost',
-  port:
-    (process.env.DATABASE_PORT && parseInt(process.env.DATABASE_PORT)) || 5433,
+  port:parseInt(process.env.DATABASE_PORT, 10) || 5433,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   name: process.env.DATABASE_NAME,
