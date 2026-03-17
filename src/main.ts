93c82ce9cb05e22aeb39077c073a1e4ea203d9ff
IAmAdri <atoma304@gmail.com>
2026-03-17 22:24:06 +0200

    Changed some environment variables

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //Add middlewares
   appCreate(app);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
   //console.log(app);
-  console.log('DB_PASSWORD din ENV este:', process.env.DB_PASSWORD);
-console.log('DB_PORT din ENV este:', process.env.DB_PORT);
+  
 }
 bootstrap();
